ac94b2c2ecdbc0677de7ec153165c7ad7785d586
RodolphoVSantoro <rodolphovs@gmail.com>
2023-04-10 22:42:32 -0300

    fix: remove unused import from rust example

--- rust-lib/src/main.rs
+++ rust-lib/src/main.rs
@@ -1,6 +1,4 @@
-use std::{ffi::c_char, os::raw::c_uchar};
-
-use rust_lib::{load_image, drop_image};
+use rust_lib::{drop_image, load_image};
 
 fn main() {
     let path = std::ffi::CString::new("./2k.png").unwrap();
@@ -13,6 +11,4 @@ fn main() {
         println!("Hello, world!{:?}", unsafe { (**pixels).red });
         drop_image(image)
     }
-
-    // println!("Hello, world!{:?}", unsafe {(**image).red});
 }
